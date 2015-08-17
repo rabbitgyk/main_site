3c4b11729e31f358d2fed9b6390a4b72623bd6a7
guoyankui <yankuiguo@gmail.com>
2015-08-17 18:59:45 +0800

    添加 service层

---
+++ src/main/java/com/rabbit/site/service/BaseUserService.java
@@ -0,0 +1,9 @@
+package com.rabbit.site.service;
+/**
+ *
+ * @author rabbit
+ * @date 2015年8月17日 下午6:58:46
+ */
+public class BaseUserService {
+
+}
---
+++ src/test/java/com/rabbit/site/service/BaseUserServiceTest.java
@@ -0,0 +1,19 @@
+package com.rabbit.site.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author rabbit
+ * @date 2015年8月17日 下午6:59:06
+ */
+public class BaseUserServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
