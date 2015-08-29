1520e829f769bfe501e25680614e64fa4ad13503
rabbitgyk <yankuiguo@gmail.com>
2015-08-30 00:39:56 +0800

    添加数据库的sql语句

---
+++ src/main/resources/jingjing.sql
@@ -0,0 +1,17 @@
+insert into baseuser (userName, password, extra) values 
+('rabbit', 'jfe43439iirei4irie4i3i5453343iis', 'user is a person hey'), 
+('qianjin', 'jfe43439iirei4irie4i3i5453343iis', 'user is a person hey');
+
+alter table user add salt char(8);
+
+alter table user rename BaseUser;
+
+update user set salt='12345678';
+
+select userName from baseuser where userId='1';
+
+select 1 from dual;
+
+CREATE TABLE baseuser (userId int NOT NULL AUTO_INCREMENT, userName varchar(50), password char(32), extra varchar(100), salt char(8), PRIMARY KEY (userId)) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+INSERT INTO baseuser (userId, userName, password, extra, salt) VALUES (0, '', '', '', '');
