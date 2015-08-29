f1ce4745ae2ff62e65bf9eb02b31065d3b54ffe3
rabbitgyk <yankuiguo@gmail.com>
2015-08-30 00:24:30 +0800

    添加mysql数据库，并编写spring jdbc dao层

--- src/main/java/com/rabbit/site/service/UserLoginService.java
+++ src/main/java/com/rabbit/site/service/UserLoginService.java
@@ -4,6 +4,8 @@ package com.rabbit.site.service;
  * @author rabbit
  * @date 2015年8月17日 下午6:58:46
  */
-public class BaseUserService {
+public interface UserLoginService {
+	
+	public String getSalt(String userName);
 
 }
